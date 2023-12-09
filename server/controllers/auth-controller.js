3bf8b60944c5df6b790fc8e13b0fe0d67e7357fb
Joynal Abedin <cfcfahim26@gmail.com>
2023-12-09 22:12:00 +0600

    regsitration added

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const User = require('../models/user-model');
 
 const postRegister = async (req, res) => {
   try {
     console.log(req.body);
-    return res.status(200).json({ msg: 'Welcome to the authentication page' });
+    const user = await User.create(req.body);
+    return res
+      .status(200)
+      .json({ msg: 'Welcome to the authentication page', user: user });
   } catch (error) {
     return res.status(404).json({ msg: 'Page not found!' });
   }
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -5,12 +5,35 @@ const UserSchema = new Schema({
     type: String,
     required: [true, 'You must provide a valid email!'],
     trim: true,
-    maxlength: [20, 'Task name cannot be more than 20 characters'],
+    maxlength: [20, 'Your email cannot be more than 20 characters'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'You must provide a valid email!',
+    ],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'You must provide a valid username'],
+    trim: true,
+    unique: true,
+    maxlength: [20, 'Your username cannot be more than 20 characters'],
+  },
+  contact: {
+    type: Number,
+    required: [true, 'You must provide a valid number'],
+    trim: true,
   },
   password: {
     type: String,
     required: [true, 'You must provide a valid password'],
     trim: true,
-    maxlength: [20, 'Task name cannot be more than 20 characters'],
+    maxlength: [20, 'Your password cannot be more than 20 characters'],
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
   },
 });
+
+module.exports = model('User', UserSchema);
