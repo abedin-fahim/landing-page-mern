a181cbdb4627fbda242933378fc5aa9afc20fbb0
Joynal Abedin <cfcfahim26@gmail.com>
2023-12-09 23:30:36 +0600

    new update

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -3,8 +3,16 @@ const User = require('../models/user-model');
 
 const postRegister = async (req, res) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
+    // Check if the email already exists / Not necessary since email is required in the User model
+    // const isEmailExist = await User.findOne({ email: req.body.email });
+
+    // if (!isEmailExist) {
+    //   const user = await User.create(req.body);
+    // }
+    
     const user = await User.create(req.body);
+
     return res
       .status(200)
       .json({ msg: 'Welcome to the authentication page', user: user });
