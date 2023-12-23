50452cac601098f6f810caeef6bf45c9da69fc08
Joynal Abedin <cfcfahim26@gmail.com>
2023-12-23 19:58:09 +0600

    error handling middlewawre added

--- server/middlewares/error-middleware.js
+++ server/middlewares/error-middleware.js
@@ -1,8 +1,8 @@
 const errorMiddleware = (err, req, res, next) => {
   // console.log(err?.status, err?.msg);
-  const status = err.status | 500;
-  const msg = err.msg | 'Server Error';
-  const info = err.info | 'Currently facing technical difficulties!';
+  const status = err.status || 500;
+  const msg = err.msg || 'Server Error';
+  const info = err.info || 'Currently facing technical difficulties!';
 
   return res.status(status).json({ msg, info });
 };
